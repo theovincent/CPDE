b297796b37f90e41b600db972d958a3ee7781581
Théo <theo.vincent@eleves.enpc.fr>
2022-03-23 17:41:45 +0100

    Clean repo

--- ruptures_changing/__init__.py
+++
@@ -1,6 +0,0 @@
-r"""Search methods"""
-
-from .dynpensembling import DynpEnsembling
-from .windowensembling import WindowEnsembling
-from .binsegensembling import BinsegEnsembling
-from .bottomupensembling import BottomUpEnsembling
