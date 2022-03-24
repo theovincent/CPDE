f8ca60929d85dbcf1998a5657e0a54eab5e31203
Théo <theo.vincent@eleves.enpc.fr>
2022-03-24 13:39:21 +0100

    DynpEnsemble with cost_factory

--- ensemble_methods/dynamic_programming_ensemble.py
+++ ensemble_methods/dynamic_programming_ensemble.py
@@ -3,7 +3,9 @@ from functools import lru_cache
 import numpy as np
 
 from ruptures.utils import sanity_check
+from ruptures.costs import cost_factory
 from ruptures.detection.dynp import Dynp
+from ruptures.exceptions import BadSegmentationParameters
 
 
 class DynpEnsemble(Dynp):
@@ -14,21 +16,31 @@ class DynpEnsemble(Dynp):
     sum of errors is minimum.
     """
 
-    def __init__(self, model="l2", custom_cost=None, min_size=2, jump=5, params=None, scale_aggregation=lambda array: array):
+    def __init__(self, models=["l2"], min_size=2, jump=5, params=None, scale_aggregation=lambda array: array):
         """Creates a Dynp instance.
 
         Args:
-            model (str, optional): segment model, ["l1", "l2", "rbf"]. Not used if ``'custom_cost'`` is not None.
-            custom_cost (BaseCost, optional): custom cost function. Defaults to None.
+            models (list[str], optional): segment model, [["l1", "l2"], ["rbf"]].
             min_size (int, optional): minimum segment length.
             jump (int, optional): subsample (one every *jump* points).
             params (dict, optional): a dictionary of parameters for the cost instance.
+            scale_aggregation (collable): aggregation function
         """
-        super(DynpEnsemble, self).__init__(model, custom_cost, min_size, jump, params)
+        self.model_names = models
+        self.costs = []
+        self.min_size = min_size
+        for model in models:
+            if params.get(model, None) is None:
+                self.costs.append(cost_factory(model=model))
+            else:
+                self.costs.append(cost_factory(model=model, **params[model]))
+            self.min_size = max(self.min_size, self.costs[-1].min_size)
+        self.jump = jump
+        self.n_samples = None
         self.scale_aggregation = scale_aggregation
 
     @lru_cache(maxsize=None)
-    def _seg(self, start, end, n_bkps):
+    def seg(self, start, end, n_bkps):
         """Recurrence to find the optimal partition of signal[start:end].
 
         This method is to be memoized and then used.
@@ -44,7 +56,10 @@ class DynpEnsemble(Dynp):
         jump, min_size = self.jump, self.min_size
 
         if n_bkps == 0:
-            cost = self.cost.error(start, end)
+            cost = [
+                self.costs[idx_cost].error(start, end)
+                for idx_cost in range(len(self.costs))
+            ]
             return {(start, end): cost}
         elif n_bkps > 0:
             # Let's fill the list of admissible last breakpoints
@@ -79,7 +94,6 @@ class DynpEnsemble(Dynp):
                 tmp_partition[(bkp, end)] = right_partition[(bkp, end)]
                 sub_problems.append(tmp_partition)
 
-            #---------NEW PART----------from here
             #number of cost functions used in an ensemble
             number_of_costs = len(tmp_partition[(bkp, end)])
             #ensembling
@@ -87,4 +101,52 @@ class DynpEnsemble(Dynp):
 
             # Find the optimal partition
             return sub_problems[np.argmin(self.scale_aggregation(array))]
-            #---------NEW PART----------till here
+
+
+    def fit(self, signal) -> "Dynp":
+        """Create the cache associated with the signal.
+
+        Dynamic programming is a recurrence; intermediate results are cached to speed up
+        computations. This method sets up the cache.
+
+        Args:
+            signal (array): signal. Shape (n_samples, n_features) or (n_samples,).
+
+        Returns:
+            self
+        """
+        # clear cache
+        self.seg.cache_clear()
+        # update some params
+        [self.costs[idx_cost].fit(signal) for idx_cost in range(len(self.costs))]
+        self.n_samples = signal.shape[0]
+        return self
+
+    def predict(self, n_bkps):
+        """Return the optimal breakpoints.
+
+        Must be called after the fit method. The breakpoints are associated with the signal passed
+        to [`fit()`][ruptures.detection.dynp.Dynp.fit].
+
+        Args:
+            n_bkps (int): number of breakpoints.
+
+        Raises:
+            BadSegmentationParameters: in case of impossible segmentation
+                configuration
+
+        Returns:
+            list: sorted list of breakpoints
+        """
+        # raise an exception in case of impossible segmentation configuration
+        if not sanity_check(
+            n_samples=self.costs[0].signal.shape[0],
+            n_bkps=n_bkps,
+            jump=self.jump,
+            min_size=self.min_size,
+        ):
+            raise BadSegmentationParameters
+        partition = self.seg(0, self.n_samples, n_bkps)
+        bkps = sorted(e for s, e in partition.keys())
+        return bkps
+    
